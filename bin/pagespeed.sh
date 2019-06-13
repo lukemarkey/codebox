d6fbfa1d1d4e80166b23a73d15be00ee18fc37ae
Luke Markey <luke.h.markey@gmail.com>
2019-06-13 08:44:32 -0400

    update

--- bin/pagespeed.sh
+++ bin/pagespeed.sh
@@ -3,6 +3,4 @@
 WEBSITE=()
 WEBSITE+=('https://aimresearchservices.com/')
 
-API_KEY='AIzaSyC1HpXKskB1L26v3EvPI4VQLbg3hJz_yOc'
-
 curl https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=https://aimresearchservices.com/&key=$API_KEY
