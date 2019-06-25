e4f07b9db62f3703f3f9c4e3f58d745d5c24ab37
Luke Markey <luke.h.markey@gmail.com>
2019-06-25 12:00:01 -0400

    automated

--- scripts/wpcli.sh
+++ scripts/wpcli.sh
@@ -4,3 +4,8 @@ curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.pha
 php wp-cli.phar --info
 chmod +x wp-cli.phar
 sudo mv wp-cli.phar /usr/local/bin/wp
+
+## WP SEARCH REPLACE
+
+wp search-replace 'http://lsi.markey.agency' 'https://all-synced.com'
+wp search-replace 'https://lsi.markey.agency' 'https://all-synced.com'
